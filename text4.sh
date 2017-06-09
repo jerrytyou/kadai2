24fd32d360e3d2ac90ceceae1f39ccc88e4d25fe
jerrytyou <jerrytyou@gmail.com>
2017-06-09 19:23:57 +0900

    Create text4.sh

--- text4.sh
+++ text4.sh
@@ -9,7 +9,7 @@ ERROR_EXIT () {
 }
 
 echo "USAGE:./kadai2.sh int1 int2" > $tmp-ans
-./kadai2.sh aaa rrr > $tmp-err && ERROR_EXIT "TEST4-1"
+./kadai2.sh aaa rrr > $tmp-err || ERROR_EXIT "TEST4-1"
 diff $tmp-ans $tmp-err || ERROR_EXIT "TEST4-2"
 
 echo OK
